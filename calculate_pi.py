59e5fe914a48120e39939923881796a42b4f85b3
satudaku <satoo.daikii@gmail.com>
2021-07-06 16:35:23 +0800

    Update calculate_pi.py

--- calculate_pi.py
+++ calculate_pi.py
@@ -1,39 +1,44 @@
 import random
-import matplotlib.pyplot as plt     #install library 'pip install matplotlib'
+import matplotlib.pyplot as plt  # install library 'pip install matplotlib'
 
 
 def calculate_pi(n):
     num_point_total = 0
     num_point_circle = 0
-    circle = plt.Circle((0, 0), 1, color='b')   #circle center @ (0,0) with radius of 1 unit
-    fig, ax = plt.subplots()    #create one figure and subplots
-    ax.set_xlim(-1, 1)      #set x-axis and y-axis from -1 to 1
-    ax.set_ylim(-1, 1)      
-    ax.set_box_aspect(1)    #without this the circle would looks like oval
+    circle = plt.Circle((0, 0), 1, color='b')    # circle center @ (0,0) with radius of 1 unit
+    fig, ax = plt.subplots()  # create one figure and subplots
+    ax.set_xlim(-1, 1)  # set x-axis and y-axis from -1 to 1
+    ax.set_ylim(-1, 1)
+    ax.set_box_aspect(1)  # without this the circle would looks like oval
     ax.add_patch(circle)
     ax.grid(True)
 
-    for i in range(n):      
-        x = random.uniform(-1,1)    #set (x,y) coordinate to random units between -1 to 1
-        y = random.uniform(-1,1)
-        distance = x**2 + y**2      #distance between center of the firgure to the point
-        
-        if distance <= 1 and distance >= -1:    #weather the distance is within the circle or not
+    for i in range(n):
+        # set (x,y) coordinate to random units between -1 to 1
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        distance = x**2 + y**2  # distance between center of the firgure to the point
+
+        if distance <= 1 and distance >= -1:  # weather the distance is within the circle or not
             num_point_circle += 1
-            ax.plot(x, y, 'o', color='r')   #point within the circle
+            ax.plot(x, y, 'o', color='r')  # point within the circle
 
         else:
-            ax.plot(x, y, 'o', color='y')   #point outside the circle
+            ax.plot(x, y, 'o', color='y')  # point outside the circle
 
         num_point_total += 1
-    
-    return print(4 * num_point_circle / num_point_total)    #use equation to get pi
+
+    pi = 4 * num_point_circle / num_point_total  # use equation to get pi
+    plt.figtext(0.02, 0.95, 'π = ' + str(pi))
+    print(pi)
+
+    return
 
 
 def input_num_point():
-    print ('Calculation to find the value of pi using random points between 0 to 1')
     try:
-        num_point = int(input('Input number of points (int) = '))     #input error handling
+        # input error handling
+        num_point = int(input('Input number of points (int) = '))
 
     except ValueError:
         print('>>>>ERROR!!! input only integers!')
@@ -41,7 +46,8 @@ def input_num_point():
 
     calculate_pi(num_point)
 
-    return plt.show()       #show the figure
+    return plt.show()  # show the figure
 
-                
+
+print('Calculation to find the value of pi using random points between 0 to 1')
 input_num_point()
